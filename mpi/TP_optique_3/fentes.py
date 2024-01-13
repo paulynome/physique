89713b2b51c3cad1d6d4875a6248438b7ceb6d27
noah <noah.gounet19@orange.fr>
2024-01-13 20:22:20 +0100

    ZSCORE

--- mpi/TP_optique_3/fentes.py
+++ mpi/TP_optique_3/fentes.py
@@ -1,31 +1,28 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-D = [400,500,600,700,800,900,1000,1100,1200]
-R =[4, 5, 6, 6.5, 8, 9, 10, 11.5,12.5]
+D = [0.4,0.5,0.6,0.7,0.8,0.9,1.0,1.1,1.2]
+R =[4*10**(-3), 5*10**(-3), 6*10**(-3), 6.5*10**(-3), 8*10**(-3), 9*10**(-3), 10*10**(-3), 11.5*10**(-3),12.5*10**(-3)]
 
 plt.title("R en fonction de D")
 
 plt.plot(D,R, 'r+')
 plt.xlabel("D")
 plt.ylabel("R")
-uy = 1
-plt.errorbar(D, R, xerr = 0, yerr = uy, fmt = 'o')
+ur = 1*10**(-3)
+plt.errorbar(D, R, xerr = 0, yerr = ur, fmt = 'o')
 plt.grid()
-
-
-
-
 model = np.polyfit(D, R, 1)
 #print(model)
-
 ymodel = model[0]*(np.array(D)) + model[1]
 
 b = 650*10**(-9) / model[0]
 print("b = ", b)
-zscore = (60*10**(-6) - b*10**(-2))/ (uy/100)
-print("zscore = ", zscore)
-
-
 plt.plot(D, ymodel, 'b:')
 plt.show()
+
+#calcul Z score
+for i in range (9) : 
+    Rreg = model[0] * D[i] 
+    zscore = (R[i] - Rreg)/ur
+    print("zscore = ", zscore)
